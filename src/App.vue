e0ee8f246807ee6d5ce5b3f71e71b74ed91ca078
simon <simon51100@gmail.com>
2020-04-04 15:37:04 +0200

    Init
    Init of project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar dense :app="true" color="primary" dark>
+      <v-toolbar-title class="headline text-uppercase">
+        <v-icon>mdi-qrcode</v-icon>Lecteur QRCode
+
+      </v-toolbar-title>
+    </v-app-bar>
+     <v-content>
+    <v-container fluid>
+      <div v-if="bottomNav == 'home'">
+      <p>Cette application a pour but de permettre la lecture de QRCode. Elle ne comporte aucun tracker. Sous peu elle sera facilement installable pour être utilisée hors-ligne !</p>
+      </div>
+<div v-if="bottomNav == 'read'">
+  <qrcode-stream @decode="onDecode"></qrcode-stream>
+
+</div>
+<div v-if="bottomNav == 'other'">
+  <h2>A propos</h2>
+
+  <p>Application développée par <a href="https://twitter.com/__si_mon" target="_blank">Simon</a>.</p>
+  <p>Elle utilise les projets open-source <a href="https://vuejs.org/" target="_blank">Vue.js</a> &amp; <a href="http://vuetifyjs.com/" target="_blank">Vuetify</a></p>
+  <p>Cette application ne comporte aucun tracker</p>
+</div>
+    </v-container>
+  </v-content>
+    <v-bottom-navigation
+    background-color="primary"
+    dark
+    app
+    v-model="bottomNav"
+  >
+  <v-btn value="home">
+    <span>Accueil</span>
+    <v-icon>mdi-home</v-icon>
+  </v-btn>
+    <v-btn value="read">
+      <span>Lire</span>
+      <v-icon>mdi-qrcode</v-icon>
+    </v-btn>
+    <v-btn value="other">
+      <span>A propos</span>
+      <v-icon>mdi-information</v-icon>
+    </v-btn>
+
+  </v-bottom-navigation>
+ </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { QrcodeStream } from 'vue-qrcode-reader'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {QrcodeStream},
+  data () {
+  return {
+    bottomNav: 'recent'
   }
+},
+methods: {
+  onDecode(s) {
+    alert(s);
+    console.log(s);
+  }
+}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const CompressionPlugin = require('compression-webpack-plugin');
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+
+module.exports = {
+  chainWebpack(config) {
+    //config.plugins.delete('prefetch');
+    config.plugin('CompressionPlugin').use(CompressionPlugin);
+    config.plugin('VuetifyLoaderPlugin').use(VuetifyLoaderPlugin);
+    //config.plugins.delete('preload');
+  },
+  pluginOptions: {
+      webpackBundleAnalyzer: {
+        openAnalyzer: true,
+         analyzerPort: 4000,
+      }
+    },
+  devServer: { https: true },
+  pwa: {
+    themeColor: "#880e4f"
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
